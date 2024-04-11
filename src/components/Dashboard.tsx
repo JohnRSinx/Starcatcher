2cd4bfb3647fd856dd3897644ce99949a5e1dfeb
John Rodrigues <johnrodrigues97106@gmail.com>
2024-04-11 15:16:06 -0300

    finshed layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,20 +1,50 @@
-
+import { useEffect, useState } from "react";
 import { Main } from "./Main";
 import { Sidebar } from "./Sidebar";
+import axios from "axios";
+import { Rocket } from "lucide-react";
 
-
-export function Dashboard() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <header className="flex items-center justify-center h-14 px-4 border-b gap-2 sm:h-16 sm:px-6 ">
-        <h1 className="text-2xl font-semibold">Starcatcher</h1>
-      </header>
-      <div className="flex flex-1">
-       <Sidebar/>
-        <Main/>
-      </div>
-    </div>
-  )
+export interface Topic {
+  id: number;
+  title: string;
+  description: string;
 }
 
+export function Dashboard() {
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [clickedTopic, setClickedTopic] = useState<Topic | undefined>();
 
+
+  async function getAllTopics() {
+    try {
+      const { data } = await axios.get<Topic[]>("http://localhost:3000/topics");
+      setTopics(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllTopics();
+  }, []);
+
+  function handleTopicClick(id: number) {
+    const clickedTopic = topics.find(topic => topic.id === id) ;
+    setClickedTopic(clickedTopic);
+  }
+  
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <header className="flex items-center h-14 px-4 border-b gap-2 sm:h-16 sm:px-6 justify-between ">
+        <a href="/">
+          <h1 className="text-2xl font-semibold">Starcatcher</h1>
+        </a>
+        <Rocket/>
+      </header>
+      <div className="flex flex-1">
+        <Sidebar topics={topics} handleTopicClick={handleTopicClick} />
+        <Main  clickedTopic={clickedTopic} />
+      </div>
+    </div>
+  );
+}
